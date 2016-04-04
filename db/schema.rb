fe097c14d551694be6f379a2f6086172de655a7d
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-04 15:35:53 -0400

    Add items table, add timestamps to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404190606) do
+ActiveRecord::Schema.define(version: 20160404193144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.string   "article_number"
+    t.string   "name",                                   null: false
+    t.string   "url"
+    t.decimal  "price",          precision: 7, scale: 2
+    t.string   "title",                                  null: false
+    t.text     "description"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "email",    null: false
-    t.string "password", null: false
+    t.string   "email",      null: false
+    t.string   "password",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
