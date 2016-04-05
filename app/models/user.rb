acc42116ac68e5e0b18a6e7a2b00972dbe357fde
Jeffrey Zhen <jeffreyzhen@Jeffreys-MacBook-Pro.local>
2016-04-05 16:08:38 -0400

    validations removed from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
