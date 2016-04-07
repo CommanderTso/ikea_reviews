3b4a116dab17586cbf0c8e4f943f8dc1cbe56f79
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-07 11:14:41 -0400

    Fixed small issues in PR for this branch.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
-FactoryGirl.create(:item)
-FactoryGirl.create(:item,
+Item.create(
   item_url: "http://www.ikea.com/us/en/catalog/products/80176284/",
   title: "HEMNES",
   subtitle: "Coffee table, black-brown",
   picture_url: "http://www.ikea.com/us/en/images/products/hemnes-coffee-table-brown__0104030_PE250678_S4.JPG",
   price: "139.00"
+)
+
+Item.create(
+  title: "EKTORP",
+  subtitle: "Footstool, Nordvalla light blue",
+  item_url: "http://www.ikea.com/us/en/catalog/products/S99129122/",
+  picture_url: "http://www.ikea.com/us/en/images/products/ektorp-footstool-blue__0386202_PE559152_S4.JPG",
+  price: "149.00"
 )
--- spec/features/user_views_index_page_spec.rb
+++ spec/features/user_views_index_page_spec.rb
@@ -8,41 +8,42 @@ require 'rails_helper'
 
 feature "User views index page to see items" do
   before(:each) do
-    FactoryGirl.create(:item)
-    FactoryGirl.create(:item,
+    @item1 = create(:item)
+    @item2 = create(
+      :item,
       item_url: "http://www.ikea.com/us/en/catalog/products/80176284/",
       title: "HEMNES",
       subtitle: "Coffee table, black-brown",
       picture_url: "http://www.ikea.com/us/en/images/products/hemnes-coffee-table-brown__0104030_PE250678_S4.JPG",
       price: "139.00"
-      )
+    )
   end
 
   scenario "User visits '/', gets redirected to /items" do
     visit root_path
 
     expect(page).to have_content "Welcome to EyeKea!"
-    expect(page).to have_content "HEMNES"
-    expect(page).to have_content "EKTORP"
+    expect(page).to have_content @item2.title
+    expect(page).to have_content @item1.title
   end
 
   scenario "User visits '/items', sees list of items & link to add more" do
     visit items_path
 
     expect(page).to have_content "Welcome to EyeKea!"
-    expect(page).to have_link "HEMNES"
-    expect(page).to have_link "EKTORP"
+    expect(page).to have_link @item1.title
+    expect(page).to have_link @item2.title
     expect(page).to have_link "Add a new item"
   end
 
   scenario "User clicks item link, gets taken to items#show page" do
     visit items_path
 
-    click_link "HEMNES"
+    click_link @item1.title
 
-    expect(page).to have_content "Coffee table, black-brown"
-    expect(page).to have_content "HEMNES"
-    expect(page).to have_css("img[src*='hemnes-coffee-table-brown__0104030_PE250678_S4.JPG']")
+    expect(page).to have_content @item1.title
+    expect(page).to have_content @item1.subtitle
+    expect(page).to have_xpath("/html/body/img[@src='#{@item1.picture_url}']")
   end
 
   scenario "Clicking Add Item takes you the user to the add item page" do
