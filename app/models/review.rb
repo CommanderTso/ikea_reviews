c8875c08d1cba1b66e5ee2f6056fe6b4293657e2
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-08 13:40:57 -0400

    Final style change to avoid hound issues.

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,11 +5,11 @@ class Review < ActiveRecord::Base
   validates :rating, presence: true, numericality: true, inclusion: { in: 1..5, message: 'must be between 1-5' }
 
   RATING_OPTIONS = [
-    ["Pick a Rating", 0],
-    ["5 - Amazing", 5],
-    ["4 - Great", 4],
-    ["3 - Mediocre", 3],
-    ["2 - Bad", 2],
-    ["1 - Awful", 1]
+    ["Pick a Rating".freeze, 0],
+    ["5 - Amazing".freeze, 5],
+    ["4 - Great".freeze, 4],
+    ["3 - Mediocre".freeze, 3],
+    ["2 - Bad".freeze, 2],
+    ["1 - Awful".freeze, 1]
   ].freeze
 end
