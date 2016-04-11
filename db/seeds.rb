8d6133f1573ed06ae0be4c97c0cd6ffdaccf4584
Ngan <Katie@Ngans-MacBook-Pro.local>
2016-04-11 16:50:42 -0400

    More changes to eliminate style & best practices errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-item_1 = Item.create(
+@item_1 = Item.find_or_create_by(
   item_url: "http://www.ikea.com/us/en/catalog/products/80176284/",
   title: "HEMNES",
   subtitle: "Coffee table, black-brown",
@@ -6,7 +6,7 @@ item_1 = Item.create(
   price: "139.00"
 )
 
-item_2 = Item.create(
+@item_2 = Item.find_or_create_by(
   title: "EKTORP",
   subtitle: "Footstool, Nordvalla light blue",
   item_url: "http://www.ikea.com/us/en/catalog/products/S99129122/",
@@ -14,16 +14,19 @@ item_2 = Item.create(
   price: "149.00"
 )
 
-user_1 = User.create(email: "abc@gmail.com", password: "12345678")
+@user_1 = User.find_by(email: "abc@gmail.com")
+unless @user_1.present?
+  @user_1 = User.create(email: "abc@gmail.com", password: "12345678", password_confirmation: "12345678")
+end
 
 7.times do
-  Review.create(rating: 3, description: "Cool", item: item_1, user: user_1)
-  Review.create(rating: 2, description: "Nah", item: item_1, user: user_1)
-  Review.create(rating: 1, description: "Awful", item: item_1, user: user_1)
-  Review.create(rating: 5, description: "Cool", item: item_1, user: user_1)
+  Review.find_or_create_by(rating: 3, description: "Cool", item: @item_1, user: @user_1)
+  Review.find_or_create_by(rating: 2, description: "Nah", item: @item_1, user: @user_1)
+  Review.find_or_create_by(rating: 1, description: "Awful", item: @item_1, user: @user_1)
+  Review.find_or_create_by(rating: 5, description: "Cool", item: @item_1, user: @user_1)
 
-  Review.create(rating: 3, description: "Cool", item: item_2, user: user_1)
-  Review.create(rating: 2, description: "Nah", item: item_2, user: user_1)
-  Review.create(rating: 1, description: "Awful", item: item_2, user: user_1)
-  Review.create(rating: 5, description: "Cool", item: item_2, user: user_1)
+  Review.find_or_create_by(rating: 3, description: "Cool", item: @item_2, user: @user_1)
+  Review.find_or_create_by(rating: 2, description: "Nah", item: @item_2, user: @user_1)
+  Review.find_or_create_by(rating: 1, description: "Awful", item: @item_2, user: @user_1)
+  Review.find_or_create_by(rating: 5, description: "Cool", item: @item_2, user: @user_1)
 end
