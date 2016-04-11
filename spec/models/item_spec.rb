4c9cc0383553d957f2a1e26b98026d14057843fa
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-11 16:03:00 -0400

    Add unit test for new item model method for calculating average review score

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,12 +1,21 @@
 describe Item do
   before(:each) do
-    FactoryGirl.create(:item)
+    @item = create(:item_with_3_reviews)
   end
 
   describe "#price" do
     it "returns the price with two decimal places of precision" do
-      item = Item.last
-      expect(item.price).to eq("149.00")
+      expect(@item.price).to eq("149.00")
+    end
+  end
+
+  describe "#calculate_average_review_score" do
+    it "accurately averages all attached review scores" do
+      reviews = @item.reviews
+      rating_sum = @item.reviews.inject(0) { |sum, review| sum + review.rating }
+      average = (rating_sum.to_f / reviews.count).round(2)
+
+      expect(@item.calculate_average_review_score).to eq(average)
     end
   end
 end
