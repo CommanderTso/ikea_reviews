233ab869cb8a6427f6e44dd2217b1fa1b9687359
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-11 14:17:21 -0400

    Factor out calculate_average_review to the item model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,15 +50,9 @@ class ItemsController < ApplicationController
   def set_show_headline(item, review_count)
     headline = "Reviews"
     if review_count > 0
-      headline += " - Average Review Score: #{calculate_average_review(item)}"
+      headline += " - Average Review Score: #{item.calculate_average_review_score}"
     end
     headline
-  end
-
-  def calculate_average_review(item)
-    rating_sum = item.reviews.pluck(:rating).inject { |sum, n| sum + n }
-    rating_count = item.reviews.count
-    (rating_sum.to_f / rating_count).round(2)
   end
 
   def create_item(item, url, item_params)
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,10 @@ class Item < ActiveRecord::Base
   def price
     sprintf('%.2f', read_attribute(:price))
   end
+
+  def calculate_average_review_score
+    rating_sum = reviews.inject(0) { |sum, review| sum + review.rating }
+    rating_count = reviews.count
+    (rating_sum.to_f / rating_count).round(2)
+  end
 end
