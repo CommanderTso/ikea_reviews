4a9e1d5a362aa48f22edcf87ada5aa0a22f9f372
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-14 17:55:50 -0400

    Fix a few more style errors in the JS

--- app/assets/javascripts/vote_ajax.js
+++ app/assets/javascripts/vote_ajax.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  $("a.vote_change").click(function(event) {
+  $('a.vote_change').click(function(event) {
     event.preventDefault();
 
     var request = $.ajax({
-			method: "GET",
+			method: 'GET',
 			url: $(this).attr('ajax_path')
 		});
 
     request.done(function(data) {
       if ('new_score' in data) {
-        $('li#review-rating-' + data.review_id).text("Review rating: " + data.new_score);
+        $('li#review-rating-' + data.review_id).text('Review rating: ' + data.new_score);
       } else {
         if ($('div.flash_message').text != data.login_error) {
            $('div.flash_message').text(data.login_error)
