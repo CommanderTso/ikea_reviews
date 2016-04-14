72966898d94c692ff5a02616c8ee64a89daec690
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-14 16:52:12 -0400

    Removing sleep statements since now more tests fail

--- spec/features/user_votes_on_a_review_with_js_spec.rb
+++ spec/features/user_votes_on_a_review_with_js_spec.rb
@@ -19,7 +19,6 @@ feature "User votes on a review (with AJAX)" do
       expect(page).to have_content "We'd love your review of the"
       expect(page).to have_content("Review rating: 0")
       first(".upvote-0").click
-      sleep 1
 
       expect(page).to have_content("Review rating: 1")
     end
@@ -29,7 +28,6 @@ feature "User votes on a review (with AJAX)" do
     expect_no_page_reload do
       expect(page).to have_content("Review rating: 0")
       first(".downvote-0").click
-      sleep 1
 
       expect(page).to have_content("Review rating: -1")
     end
@@ -38,11 +36,9 @@ feature "User votes on a review (with AJAX)" do
   scenario "User removes their upvote", js: true do
     expect_no_page_reload do
       first(".upvote-0").click
-      sleep 1
       expect(page).to have_content("Review rating: 1")
 
       first(".upvote-0").click
-      sleep 1
       expect(page).to have_content("Review rating: 0")
     end
   end
@@ -50,11 +46,9 @@ feature "User votes on a review (with AJAX)" do
   scenario "User removes their downvote", js: true do
     expect_no_page_reload do
       first(".downvote-0").click
-      sleep 1
       expect(page).to have_content("Review rating: -1")
 
       first(".downvote-0").click
-      sleep 1
       expect(page).to have_content("Review rating: 0")
     end
   end
@@ -62,11 +56,9 @@ feature "User votes on a review (with AJAX)" do
   scenario "User changes their downvote to an upvote", js: true do
     expect_no_page_reload do
       first(".downvote-0").click
-      sleep 1
       expect(page).to have_content("Review rating: -1")
 
       first(".upvote-0").click
-      sleep 1
       expect(page).to have_content("Review rating: 1")
     end
   end
@@ -74,11 +66,9 @@ feature "User votes on a review (with AJAX)" do
   scenario "User changes their upvote to an downvote", js: true do
     expect_no_page_reload do
       first(".upvote-0").click
-      sleep 1
       expect(page).to have_content("Review rating: 1")
 
       first(".downvote-0").click
-      sleep 1
       expect(page).to have_content("Review rating: -1")
     end
   end
